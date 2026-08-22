8d11906249e75a611c15e29665ddfdbd27343d6e
khatunajannat <khatunajannat35@gmail.com>
2026-08-23 01:21:43 +0600

    missed arrow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
           </span>
           <svg
             className="ml-2 w-5 h-5"
-            fill="currentColor"
+            fill="slate-800"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
