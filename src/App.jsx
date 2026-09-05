434ffd851cc8f3a64dfe77b66db972a980a14ab3
khatunajannat <khatunajannat35@gmail.com>
2026-09-06 00:52:42 +0600

    padding in login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Signup from "./pages/Signup";
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-stone-200 text-slate-800 pt-16">
+      <div className="min-h-screen bg-stone-200 text-slate-800 pt-17">
         <Navbar />
 
         <Routes>
